22050fa1c056b04e3297c1b9caab0eef64687865
Luellle X <rosewang0207@outlook.com>
2025-11-17 22:50:01 -0800

    add diy theme

--- files/theme.js
+++ files/theme.js
@@ -14,3 +14,30 @@ darkmodeButton.addEventListener("click", () => {
         darkmodeButton.innerText = "Light";
     }
 });
+
+const themeButton = document.getElementById("diy_button");
+const modal = document.getElementById("theme_modal");
+const closeModal = document.getElementById("close_modal");
+const bgInput = document.getElementById("bg_color");
+const textInput = document.getElementById("text_color");
+const fontInput = document.getElementById("font_select");
+
+themeButton.addEventListener("click", () => {
+    modal.style.display = "flex";
+});
+
+closeModal.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+bgInput.addEventListener("input", (e) => {
+    document.documentElement.style.setProperty('--main-color', e.target.value);
+});
+
+textInput.addEventListener("input", (e) => {
+    document.documentElement.style.setProperty('--paragraph-color', e.target.value);
+});
+
+fontInput.addEventListener("change", (e) => {
+    document.documentElement.style.setProperty('--content-font', e.target.value);
+});
